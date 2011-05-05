e8233f4c35dbe2c389f10df0b5cc5e49b019be6d
Chris Dzombak <chris@chrisdzombak.net>
2011-05-05 18:30:39 -0400

    Initial commit of APB3-CSR bridge and its docs

---
+++ apb3_csr_bridge-indirect/doc/README.md
@@ -0,0 +1,35 @@
+# APB3 - CSR Bridge
+
+Verilog bridge between an ARM APB3 master and a CSR bus slave.
+
+## Indirect Addressing
+
+This variant of the bridge uses an "indirect" address scheme where addresses don't map directly from the APB address space to the CSR address space. In an ideal world, we could simply use the lower 14 bits of the APB address as the CSR address, but in some situations (particularly when using the MSS on a Microsemi SmartFusion device) that much address space is not available on the APB.
+
+This bridge has a two-register interface. The registers are mapped into the processor's memory.
+
+* Read/write to address 0 to set or retrieve the (14 bit) CSR target address.
+* Read/write to address 4 to perform a (32 bit) read/write on the CSR bus using the address you previously wrote into the bridge.
+
+## Background Information
+
+ARM APB3 is used to interface to peripherals that are low bandwidth and don't require the high performance of a pipelined bus interface.
+
+* [APB3 Documentation from ARM](http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ihi0024b/index.html) (requires registration)
+* [Microsemi AMBA Interfacing Guide](http://www.actel.com/documents/SmartFusion_Build_APB3core_AN.pdf) (has an overview of APB3 interfacing and doesn't require registration)
+
+The CSR bus is part of the open-source Milkymist SoC.
+
+* [Documentation](http://www.milkymist.org/socdoc/csr.pdf)
+* [Milkymist SoC](http://www.milkymist.org/mmsoc.html)
+* [Milkymist on Github](https://github.com/milkymist)
+
+# Feedback
+
+Any feedback is welcomed, particularly bug reports or suggestions for improvement. You can message [me](https://github.com/cdzombak) on Github, email me, [open an issue](https://github.com/cdzombak/useful-hdl/issues), or fork this project and open a pull request.
+
+# Author
+
+This module was written by [Chris Dzombak](http://chris.dzombak.name).
+
+It was originally used in a UMich EECS373 project (link TK) to interface with the DMX cores from the Milkymist project.
---
+++ apb3_csr_bridge-indirect/rtl/apb_csr_bridge.v
@@ -0,0 +1,170 @@
+// apb_csr_bridge.v
+
+// Bridge between an ARM APB3 master and a CSR slave.
+// Uses an indirect addressing scheme.
+// For complete docs, source, and additional info, see:
+//   http://github.com/cdzombak/useful-hdl/apb3_csr_bridge-indirect
+
+// Originally developed for a UMich EECS373 project:
+//   < link TK >
+// By Chris Dzombak
+//   http://chris.dzombak.name
+//   chris@chrisdzombak.net
+
+module apb_csr_bridge(
+  PCLK,
+  PADDR,
+  PENABLE,
+  PSEL,
+  PRESERN,
+  PWRITE,
+  PREADY,
+  PSLVERR,
+  PWDATA,
+  PRDATA,
+  CSR_A,
+  CSR_WE,
+  CSR_DW,
+  CSR_DR
+  );
+
+input PCLK, PENABLE, PSEL, PRESERN, PWRITE;
+input  [31:0] PADDR;
+input  [31:0] PWDATA;
+input  [31:0] CSR_DR;
+output reg [31:0] PRDATA;
+output reg [31:0] CSR_DW;
+output reg [13:0] CSR_A;
+output reg PREADY, CSR_WE;
+output PSLVERR;
+
+wire rd_enable;
+wire wr_enable;
+
+reg [13:0] addr;
+reg [1:0]  state;
+
+parameter IDLE  = 2'b00;
+parameter READ1 = 2'b01;
+parameter READ2 = 2'b10;
+parameter WRITE = 2'b11;
+
+assign wr_enable = (PENABLE && PWRITE && PSEL);
+assign rd_enable = (!PWRITE && PSEL);
+assign PSLVERR = 1'b0;
+
+always @(posedge PCLK, negedge PRESERN) begin
+	if (!PRESERN) begin
+		// reset
+		addr <= 13'b0;
+		state <= IDLE;
+		PRDATA <= 32'b0;
+		CSR_DW <= 32'b0;
+		CSR_A <= 32'b0;
+		PREADY <= 1'b1;
+		CSR_WE <= 1'b0;
+	end else begin
+		case (state)
+			IDLE:
+				if (wr_enable) begin
+					case (PADDR[2])
+						1'b0: begin
+								// writing address. save address for future use, and we're ready to go.
+								addr <= PWDATA[13:0];
+								state <= IDLE;
+                                PRDATA <= 32'b0;
+                                CSR_DW <= 32'b0;
+                                CSR_A <= 32'b0;
+								PREADY <= 1'b1;
+	                        	CSR_WE <= 1'b0;
+							end
+						1'b1: begin
+								// writing data, so kick off write to CSR.
+								CSR_A <= addr;
+								CSR_DW <= PWDATA;
+								CSR_WE <= 1'b1;
+								state <= WRITE;
+								PREADY <= 1'b0;
+                                PRDATA <= 32'b0;
+                                addr <= addr;
+							end
+					endcase
+				end else if (rd_enable) begin
+					case (PADDR[2])
+						1'b0: begin
+								// reading address register
+								PRDATA[31:14] <= 18'b0;
+								PRDATA[13:0]  <= addr;
+                                CSR_DW <= 32'b0;
+                                CSR_A <= 32'b0;
+								PREADY <= 1'b1;
+	                        	CSR_WE <= 1'b0;
+								state <= IDLE;
+                                addr <= addr;
+							end
+						1'b1: begin
+								// reading data, so kick off read from CSR
+								CSR_A <= addr;
+                                CSR_DW <= 32'b0;
+	                        	CSR_WE <= 1'b0;
+								PREADY <= 1'b0;
+                                PRDATA <= 32'b0;
+								state <= READ1;
+                                addr <= addr;
+							end
+                    endcase
+				end else begin
+                    // just idle
+                    addr <= addr;
+    				state <= IDLE;
+    				PRDATA <= 32'b0;
+    				CSR_DW <= 32'b0;
+    				CSR_A <= 32'b0;
+    				PREADY <= 1'b1;
+    				CSR_WE <= 1'b0;
+                end
+			READ1: begin
+					// we just wrote address to the CSR bus, so it will have data ready next cycle
+                    CSR_A <= addr;
+                    CSR_DW <= 32'b0;
+                	CSR_WE <= 1'b0;
+					PREADY <= 1'b0;
+                    PRDATA <= 32'b0;
+                    addr <= addr;
+					state <= READ2;
+				end
+			READ2: begin
+					// yay, CSR has data ready for us now
+					PREADY <= 1'b1;
+					PRDATA <= CSR_DR;
+                    CSR_A <= 32'b0;
+                    CSR_DW <= 32'b0;
+                	CSR_WE <= 1'b0;
+                    addr <= addr;
+					state <= IDLE;
+				end
+			WRITE: begin
+					// we just wait one cycle to give the CSR write time to complete
+					CSR_WE <= 1'b0;
+					PREADY <= 1'b1;
+                    PRDATA <= 32'b0;
+                    CSR_A <= addr;
+                    CSR_DW <= 32'b0;
+                    addr <= addr;
+					state <= IDLE;
+				end
+			default: begin
+    				// reset
+    				addr <= 13'b0;
+    				state <= IDLE;
+    				PRDATA <= 32'b0;
+    				CSR_DW <= 32'b0;
+    				CSR_A <= 32'b0;
+    				PREADY <= 1'b1;
+    				CSR_WE <= 1'b0;
+                end
+		endcase
+	end
+end
+
+endmodule
